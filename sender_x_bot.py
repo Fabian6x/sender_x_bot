d6a83cd45426f0b420d58eeefb9c993673c8e2a6
Fabián <fabian.valdes@ufrontera.cl>
2024-05-19 17:00:04 -0400

    Primera versión del sender x bot

---
+++ sender_x_bot.py
@@ -0,0 +1,31 @@
+from telegram import Bot
+from asyncio import run
+import inspect
+class CustomBot(Bot):
+    token='925207790:AAFACRkU3fSScjxaEoZRUVHF0cgDh3TGE7Y'
+    chat_id=267811391
+    def __init__(self,**kwargs):
+        print('Bot initialized')
+    def __str__(self):
+        return f'Bot information:\n - Token  : '+self.token + '\n - Chat_id: ' +str(self.chat_id)
+
+def bot_method_decorator(orig_func):
+    def decorator(*args, **kwargs):
+        async def interior(*args,**kwargs):
+            kwargs['chat_id']=args[0].chat_id
+            async with Bot(token=args[0].token) as bot:
+                return await getattr(bot, orig_func.__name__)(**kwargs)
+        return run(interior(*args,**kwargs))
+    return decorator
+# Apply the decorator to all bot methods except dunder methods
+for name, fn in inspect.getmembers(CustomBot,inspect.isfunction):
+    if not name.startswith('__'):
+        setattr(CustomBot, name, bot_method_decorator(fn))
+
+
+def main() -> None:
+    MyBot=CustomBot()
+    MyBot.send_message(text='holi4')
+
+if __name__ == "__main__":
+    main()
